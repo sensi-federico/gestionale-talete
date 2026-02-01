4358d1e22c79ac9b29523d4506fe85fd375c70a8
Federico Sensi <blasph@MacBook-Pro-di-Federico.local>
2026-02-01 12:39:22 +0100

    modal for update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "prompt",
       includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
       manifest: {
         name: "Gestionale Rilevamenti Operai",
